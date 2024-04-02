d9f9fdc48c3c0b7e0c8ed5b209312451bc460ba5
Steven V <sgvillacis@gmail.com>
2024-04-02 15:41:19 -0400

    models and serializers

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,8 @@
-from .views import Home
+from .views import Home, TeaList, TeaDetail
 from django.urls import path
 
 urlpatterns = [
   path('', Home.as_view(), name='home'),
+  path('tea/', TeaList.as_view(), name='tea-list'),
+  path('tea/<int:id>/', TeaDetail.as_view(), name='tea-detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,21 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from rest_framework import generics
+from .models import Tea
+from .serializers import TeaSerializer
+
 # Define the home view
 class Home(APIView):
   def get(self, request):
     content = {'message': 'Welcome to the teacollector api home route!'}
     return Response(content)
+  
+class TeaList(generics.ListCreateAPIView):
+  queryset = Tea.objects.all()
+  serializer_class = TeaSerializer
+
+class TeaDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Tea.objects.all()
+  serializer_class = TeaSerializer
+  lookup_field = 'id'
